c463ab0e53b856834d9cda620b52fd597a2bca62
Adrienne Galloway <aasgalloway@gmail.com>
2025-02-01 22:42:32 +0000

    hamming exercise passes all tests

--- bash/hamming/hamming.sh
+++ bash/hamming/hamming.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/env bash
 
-# The following comments should help you get started:
-# - Bash is flexible. You may use functions or write a "raw" script.
-#
-# - Complex code can be made easier to read by breaking it up
-#   into functions, however this is sometimes overkill in bash.
-#
-# - You can find links about good style and other resources
-#   for Bash in './README.md'. It came with this exercise.
-#
-#   Example:
-#   # other functions here
-#   # ...
-#   # ...
-#
-#   main () {
-#     # your main function code here
-#   }
-#
-#   # call main with all of the positional arguments
-#   main "$@"
-#
-# *** PLEASE REMOVE THESE COMMENTS BEFORE SUBMITTING YOUR SOLUTION ***
+DNA_1=$1
+DNA_2=$2
+
+if [ $# -ne 2 ]; then
+    echo "Usage: hamming.sh <string1> <string2>"
+    exit 1
+elif [ $1 == $2 ]; then
+    echo "0"
+else
+    size=${#DNA_1}
+    if [ ${#DNA_1} -ne ${#DNA_2} ]; then # ${#variable} is used to get the size of the variable
+        echo "The strands must be of equal length."
+        exit 1
+    fi
+    hamming_distance=0
+    for ((i = 0; i < size; i++)); do
+        if [ "${DNA_1:i:1}" != "${DNA_2:i:1}" ]; then
+        ((hamming_distance++))
+        fi
+    done
+    echo "$hamming_distance"
+fi 
