c4c494618561479e144e9edf19ea5b9254941fdf
Adrienne Galloway <aasgalloway@gmail.com>
2025-02-04 00:23:58 +0000

    passing 9/10 tests

--- bash/acronym/acronym.sh
+++ bash/acronym/acronym.sh
@@ -2,14 +2,17 @@
 
 text=$@
 
-# removes everything except letters
-echo $text | sed "s/[^[:alpha:]]//g"
+# removes everything except letters, replaces with space 
+text=$(echo $text | tr -d "'" | tr '[a-z]' '[A-Z]' | sed "s/[^[:alpha:]]/ /g")
+word=""
+IFS=' '
 
-# IFS=' '
+read -ra newarr <<< "$text"
 
-# read -ra newarr <<< "$text"
+for val in "${newarr[@]}";
+do
+    echo ${val:0:1} | tr -d '\n'
+done 
 
-# for val in "${newarr[@]}";
-# do
-#  echo "$val"
-# done
+# need to take care of globbing character
+# need to take care of apostrophes 
