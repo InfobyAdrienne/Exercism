db71db2f1ca654de4eab68dda0dbc078a63d5b88
Adrienne Galloway <aasgalloway@gmail.com>
2025-03-02 22:18:55 +0000

    passing 11/13 tests on leap challenge

---
+++ bash/leap/leap.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+year=$1
+
+if [ "$#" -ne 1 ]; then
+  echo "Usage: leap.sh <year>"
+  exit 1
+elif [ $((year % 100)) -eq 0 ] && [ $((year % 400)) -ne 0 ]; then 
+  echo "false"
+elif [ $((year % 4)) -eq 0 ]; then 
+  echo "true"
+else 
+  echo "false"
+fi
+  
