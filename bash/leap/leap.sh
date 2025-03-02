2ded77eba32f755284fb9f1060d7629a6e9ea6a5
Adrienne Galloway <aasgalloway@gmail.com>
2025-03-02 22:28:02 +0000

    passing 13/13 tests in leap challenge

--- bash/leap/leap.sh
+++ bash/leap/leap.sh
@@ -5,6 +5,9 @@ year=$1
 if [ "$#" -ne 1 ]; then
   echo "Usage: leap.sh <year>"
   exit 1
+elif [[ $year =~ ^[+-]?[0-9]+\.[0-9]*$ ]] || [[ $year =~ ^[a-zA-Z] ]]; then
+  echo "Usage: leap.sh <year>"
+  exit 1
 elif [ $((year % 100)) -eq 0 ] && [ $((year % 400)) -ne 0 ]; then 
   echo "false"
 elif [ $((year % 4)) -eq 0 ]; then 
@@ -12,4 +15,3 @@ elif [ $((year % 4)) -eq 0 ]; then
 else 
   echo "false"
 fi
-  
