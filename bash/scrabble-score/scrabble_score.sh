995f95a463edc260affa59cabee762d035ae37a1
Adrienne Galloway <aasgalloway@gmail.com>
2025-03-19 22:53:51 +0000

    removing unecessary arr array - passing all tests

--- bash/scrabble-score/scrabble_score.sh
+++ bash/scrabble-score/scrabble_score.sh
@@ -1,39 +1,27 @@
 #!/usr/bin/env bash
 
 val=$1
-string="${val^^}"
-length="${string}"
+string="${val^^}"  # Convert input to uppercase
+length=${#string}
+total=0  # Initialise total score
 
-arr=()
-
-for ((i = 0; i < length; i++)); do 
+for ((i = 0; i < length; i++)); do
   char="${string:i:1}"
-    if [[ "$char" =~ [AEIOULNRST] ]]; then
-      arr+=('1')
-    elif [[ $char =~ [BCMP] ]]; then
-      arr+=('3')
-    elif [[ $char =~ [DG] ]]; then
-      arr+=('2')
-    elif [[ $char =~ [FHVWY] ]]; then
-      arr+=('4')
-    elif [[ $char =~ [K] ]]; then
-      arr+=('5')
-    elif [[ $char =~ [JX] ]]; then
-      arr+=('8')
-    elif [[ $char =~ [QZ] ]]; then
-      arr+=('10')
-    else
-      arr+=('0')
-    fi
+  if [[ "$char" =~ [AEIOULNRST] ]]; then
+    (( total += 1 ))
+  elif [[ "$char" =~ [DG] ]]; then
+    (( total += 2 ))
+  elif [[ "$char" =~ [BCMP] ]]; then
+    (( total += 3 ))
+  elif [[ "$char" =~ [FHVWY] ]]; then
+    (( total += 4 ))
+  elif [[ "$char" =~ [K] ]]; then
+    (( total += 5 ))
+  elif [[ "$char" =~ [JX] ]]; then
+    (( total += 8 ))
+  elif [[ "$char" =~ [QZ] ]]; then
+    (( total += 10 ))
+  fi
 done
 
-for n in "${arr[@]}"
-do 
-  (( total += n ))
-done
-
-if [[ $total -ne '0' ]]; then 
-  echo "$total"
-else
-  echo '0'
-fi 
+echo "$total"
